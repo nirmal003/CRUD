d769c714685dbed191e9ed68ef7520d13fad7192
Nirmal <nirmalraj1025@gmail.com>
2023-02-08 22:29:17 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const app = jsonServer.create();
 
-const router = jsonServer.router("./data.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 // const middlewares = jsonServer.defaults({
